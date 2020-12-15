9421d1541de79ed85c1445b8a370cc73a659c727
Stephen Wuebker <47507003+stephenwuebker@users.noreply.github.com>
2020-12-15 16:52:13 -0500

    Create day3.sql

---
+++ day3.sql
@@ -0,0 +1,124 @@
+-- Problem is posted at https://adventofcode.com/2020/day/3
+-- Import the input data from https://adventofcode.com/2020/day/3/input
+-- Import Flat File, Single column called "Data", Table name = dbo.day3
+
+/*
+--- Day 3: Toboggan Trajectory ---
+With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's 
+very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
+
+Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+
+The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
+
+From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+
+The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........X.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...#X....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+In this example, traversing the map using this slope would cause you to encounter 7 trees.
+
+Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+*/
+
+SELECT * FROM dbo.day3
+ALTER TABLE	dbo.day3 ADD RowID INT IDENTITY(1,1)
+SELECT DISTINCT LEN(Data) FROM dbo.day3
+
+DECLARE @RowID INT
+DECLARE @MaxRows INT
+DECLARE @CharPos INT
+DECLARE @TreeCt INT
+DECLARE @RightDelta INT
+DECLARE @DownDelta INT
+
+SET @RowID = 1
+SET @CharPos = 1
+SET @TreeCt = 0
+SELECT @MaxRows = COUNT(*) FROM dbo.day3
+
+-- Set the slope
+SET @RightDelta = 3
+SET @DownDelta = 1
+
+SET @CharPos = @CharPos + @RightDelta
+SET @RowID = @RowID + @DownDelta
+
+
+WHILE @RowID <= @MaxRows
+BEGIN
+	IF @CharPos > 31
+	BEGIN
+		SET @CharPos = @CharPos - 31
+	END
+	IF (SELECT SUBSTRING(Data,@CharPos,1) FROM dbo.day3 WHERE RowID = @RowID) = '#'
+	BEGIN
+		SET @TreeCt = @TreeCt + 1
+	END
+	
+	SET @CharPos = @CharPos + @RightDelta
+	SET @RowID = @RowID + @DownDelta
+END
+
+PRINT @TreeCt
+--169
+
+/*
+--- Part Two ---
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+Right 1, down 1.
+Right 3, down 1. (This is the slope you already checked.)
+Right 5, down 1.
+Right 7, down 1.
+Right 1, down 2.
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+*/
+1,1 = 87
+3,1 = 169
+5,1 = 99
+7,1 = 98
+1,2 = 53
+
+SELECT CAST(87 AS BIGINT) * 169 * 99 * 98 * 53
+--7560370818
